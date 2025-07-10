d2c83e994e2e8b9d9e2255a96fce4b4919eeee9a
elguesabal <joseelguesabal@gmail.com>
2025-07-10 20:28:19 -0300

    adicionando uma rota simples de login para testes

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/ping", (req, res) => {
 	res.sendStatus(200);
 });
+
+app.post("/login", (req, res) => {
+	const { login, password } = req.body;
+
+	if (login === "Vampeta" && password === "123") {
+		res.status(200).send({ message: "Login bem-sucedido!" });
+	} else {
+		res.status(401).send({ message: "Credenciais inválidas!" });
+	}
+})
 
 app.listen(process.env.PORT || 3000, () => console.log("Servidor rodando"));
