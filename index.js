564d71a73cceb63a09fbf651c9d91ace2df5c5e7
elguesabal <joseelguesabal@gmail.com>
2025-07-17 20:36:12 -0300

    adicionando rota timetable-units para testes

--- index.js
+++ index.js
@@ -25,4 +25,16 @@ app.get("/training-locations", (req, res) => {
 	res.status(200).json([ "Barra da Tijuca", "Andaraí", "Madureira", "Bonsucesso", "Tijuca", "Guaratiba", "Lagoa", "Taquara"]);
 });
 
+app.get("/timetable-units", (req, res) => {
+	const units = req.query["units[]"];
+	const data = [
+		{ unit: "Taquara", address: "Rua Opnião Liberal, 315", classes: [{ day: "Segunda", start: "8:00", end: "9:30"}, { day: "Terça", start: "9:30", end: "11:00" }, { day: "Sabado", start: "11:00", end: "13:30"}, { day: "Segunda", start: "8:00", end: "9:30"}, { day: "Terça", start: "9:30", end: "11:00" }, { day: "Sabado", start: "11:00", end: "13:30"}] },
+		{ unit: "Madureira", address: "Rua Opnião Liberal, 315", classes: [{ day: "Sabado", start: "8:00", end: "9:30"}, { day: "Quinta", start: "9:30", end: "11:00"}, { day: "Quinta", start: "11:00", end: "13:30"}] },
+		{ unit: "Bonsucesso", address: "Rua Opnião Liberal, 315", classes: [{ day: "Quarta", start: "8:00", end: "9:30"}, { day: "Quarta", start: "9:30", end: "11:00"}, { day: "Quarta", start: "11:00", end: "13:30"}] },
+		{ unit: "Lagoa", address: "Rua Opnião Liberal, 315", classes: [{ day: "Sabado", start: "8:00", end: "9:30"}, { day: "Sabado", start: "9:30", end: "11:00"}, { day: "Sabado", start: "11:00", end: "13:30"}] }
+	];
+
+	res.status(200).json(data);
+});
+
 app.listen(process.env.PORT || 3000, () => console.log("Servidor rodando"));
