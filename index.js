969143a237a4cb4c3182b0609af76878fdf1fc1f
elguesabal <joseelguesabal@gmail.com>
2025-07-19 21:38:23 -0300

    fazendo pequeno ajuste na rota /timetable-units agr ela filtra por unidade e envia os horarios de acordo com as unidades pedidas

--- index.js
+++ index.js
@@ -22,17 +22,21 @@ app.post("/register", (req, res) => {
 });
 
 app.get("/training-locations", (req, res) => {
-	res.status(200).json([ "Barra da Tijuca", "Andaraí", "Madureira", "Bonsucesso", "Tijuca", "Guaratiba", "Lagoa", "Taquara"]);
+	res.status(200).json(["Barra da Tijuca", "Andaraí", "Madureira", "Bonsucesso", "Tijuca", "Guaratiba", "Lagoa", "Taquara"]);
 });
 
 app.get("/timetable-units", (req, res) => {
 	const units = req.query["units[]"];
-	const data = [
-		{ unit: "Taquara", address: "Rua Opnião Liberal, 315", classes: [{ day: "Segunda", start: "8:00", end: "9:30"}, { day: "Terça", start: "9:30", end: "11:00" }, { day: "Sabado", start: "11:00", end: "13:30"}, { day: "Segunda", start: "8:00", end: "9:30"}, { day: "Terça", start: "9:30", end: "11:00" }, { day: "Sabado", start: "11:00", end: "13:30"}] },
-		{ unit: "Madureira", address: "Rua Opnião Liberal, 315", classes: [{ day: "Sabado", start: "8:00", end: "9:30"}, { day: "Quinta", start: "9:30", end: "11:00"}, { day: "Quinta", start: "11:00", end: "13:30"}] },
-		{ unit: "Bonsucesso", address: "Rua Opnião Liberal, 315", classes: [{ day: "Quarta", start: "8:00", end: "9:30"}, { day: "Quarta", start: "9:30", end: "11:00"}, { day: "Quarta", start: "11:00", end: "13:30"}] },
-		{ unit: "Lagoa", address: "Rua Opnião Liberal, 315", classes: [{ day: "Sabado", start: "8:00", end: "9:30"}, { day: "Sabado", start: "9:30", end: "11:00"}, { day: "Sabado", start: "11:00", end: "13:30"}] }
-	];
+	const data = [];
+
+	if (units.includes("Barra da Tijuca")) data.push({ unit: "Barra da Tijuca", address: "Rua Opnião Liberal, 315", classes: [{ day: "Segunda", start: "8:00", end: "9:30" }, { day: "Terça", start: "9:30", end: "11:00" }, { day: "Sabado", start: "11:00", end: "13:30" }, { day: "Segunda", start: "8:00", end: "9:30" }, { day: "Terça", start: "9:30", end: "11:00" }, { day: "Sabado", start: "11:00", end: "13:30" }] });
+	if (units.includes("Andaraí")) data.push({ unit: "Andaraí", address: "Rua Opnião Liberal, 315", classes: [{ day: "Sabado", start: "8:00", end: "9:30" }, { day: "Quinta", start: "9:30", end: "11:00" }, { day: "Quinta", start: "11:00", end: "13:30" }] });
+	if (units.includes("Madureira")) data.push({ unit: "Madureira", address: "Rua Opnião Liberal, 315", classes: [{ day: "Sabado", start: "8:00", end: "9:30" }, { day: "Quinta", start: "9:30", end: "11:00" }, { day: "Quinta", start: "11:00", end: "13:30" }] });
+	if (units.includes("Bonsucesso")) data.push({ unit: "Bonsucesso", address: "Rua Opnião Liberal, 315", classes: [{ day: "Quarta", start: "8:00", end: "9:30" }, { day: "Quarta", start: "9:30", end: "11:00" }, { day: "Quarta", start: "11:00", end: "13:30" }] });
+	if (units.includes("Tijuca")) data.push({ unit: "Tijuca", address: "Rua Opnião Liberal, 315", classes: [{ day: "Segunda", start: "8:00", end: "9:30" }, { day: "Terça", start: "9:30", end: "11:00" }, { day: "Sabado", start: "11:00", end: "13:30" }, { day: "Segunda", start: "8:00", end: "9:30" }, { day: "Terça", start: "9:30", end: "11:00" }, { day: "Sabado", start: "11:00", end: "13:30" }] });
+	if (units.includes("Guaratiba")) data.push({ unit: "Guaratiba", address: "Rua Opnião Liberal, 315", classes: [{ day: "Quarta", start: "8:00", end: "9:30" }, { day: "Quarta", start: "9:30", end: "11:00" }, { day: "Quarta", start: "11:00", end: "13:30" }] });
+	if (units.includes("Lagoa")) data.push({ unit: "Lagoa", address: "Rua Opnião Liberal, 315", classes: [{ day: "Sabado", start: "8:00", end: "9:30" }, { day: "Sabado", start: "9:30", end: "11:00" }, { day: "Sabado", start: "11:00", end: "13:30" }] });
+	if (units.includes("Taquara")) data.push({ unit: "Taquara", address: "Rua Opnião Liberal, 315", classes: [{ day: "Segunda", start: "8:00", end: "9:30" }, { day: "Terça", start: "9:30", end: "11:00" }, { day: "Sabado", start: "11:00", end: "13:30" }, { day: "Segunda", start: "8:00", end: "9:30" }, { day: "Terça", start: "9:30", end: "11:00" }, { day: "Sabado", start: "11:00", end: "13:30" }] });
 
 	res.status(200).json(data);
 });
