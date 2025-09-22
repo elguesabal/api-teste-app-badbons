40b6939343dcb529a6b52750db2aa491da06ef4a
elguesabal <joseelguesabal@gmail.com>
2025-09-22 19:58:09 -0300

    mudando a resposta da rota /notifications e adicionando um objeto com informacoes sobre o esquema de paginamento das informacoes (ainda nao documentado no README)

--- sections/profile/listNotifications.js
+++ sections/profile/listNotifications.js
@@ -1,215 +1,227 @@
-export const listNotifications1 = [
-	{
-		id: 1,
-		viewed: false,
-		title: "NDB NEWS",
-		time: "10 min",
-		message: "Quartas de Final - Vencedor Igor Coelho"
+export const listNotifications1 = {
+	pagination: {
+		nextPage: true,
+		totalPages: 2
 	},
-	{
-		id: 2,
-		viewed: false,
-		title: "Torneio Amigo",
-		time: "15min",
-		message: "Final - Vencedor Natan Roldão"
-	},
-	{
-		id: 3,
-		viewed: false,
-		title: "Loja BadBons",
-		time: "20min",
-		message: "Camisas Novas já estão liberadas, confira!"
-	},
-	{
-		id: 4,
-		viewed: true,
-		title: "Equipe BadBons",
-		time: "20min",
-		message: "Aula de Reposição para a Tijuca, Dia 10 das 9:00 às 10:00"
-	},
-	{
-		id: 5,
-		viewed: false,
-		title: "NDB NEWS",
-		time: "10 min",
-		message: "Quartas de Final - Vencedor Igor Coelho"
-	},
-	{
-		id: 6,
-		viewed: false,
-		title: "Torneio Amigo",
-		time: "15min",
-		message: "Final - Vencedor Natan Roldão"
-	},
-	{
-		id: 7,
-		viewed: false,
-		title: "Loja BadBons",
-		time: "20min",
-		message: "Camisas Novas já estão liberadas, confira!"
-	},
-	{
-		id: 8,
-		viewed: true,
-		title: "Equipe BadBons",
-		time: "20min",
-		message: "Aula de Reposição para a Tijuca, Dia 10 das 9:00 às 10:00"
-	},
-	{
-		id: 9,
-		viewed: false,
-		title: "NDB NEWS",
-		time: "10 min",
-		message: "Quartas de Final - Vencedor Igor Coelho"
-	},
-	{
-		id: 10,
-		viewed: false,
-		title: "Torneio Amigo",
-		time: "15min",
-		message: "Final - Vencedor Natan Roldão"
-	},
-	{
-		id: 11,
-		viewed: false,
-		title: "Loja BadBons",
-		time: "20min",
-		message: "Camisas Novas já estão liberadas, confira!"
-	},
-	{
-		id: 12,
-		viewed: true,
-		title: "Equipe BadBons",
-		time: "20min",
-		message: "Aula de Reposição para a Tijuca, Dia 10 das 9:00 às 10:00"
-	},
-	{
-		id: 13,
-		viewed: false,
-		title: "NDB NEWS",
-		time: "10 min",
-		message: "Quartas de Final - Vencedor Igor Coelho"
-	},
-	{
-		id: 14,
-		viewed: false,
-		title: "Torneio Amigo",
-		time: "15min",
-		message: "Final - Vencedor Natan Roldão"
-	},
-	{
-		id: 15,
-		viewed: false,
-		title: "Loja BadBons",
-		time: "20min",
-		message: "Camisas Novas já estão liberadas, confira!"
-	}
-];
+	data: [
+		{
+			id: 1,
+			viewed: false,
+			title: "NDB NEWS",
+			time: "10 min",
+			message: "Quartas de Final - Vencedor Igor Coelho"
+		},
+		{
+			id: 2,
+			viewed: false,
+			title: "Torneio Amigo",
+			time: "15min",
+			message: "Final - Vencedor Natan Roldão"
+		},
+		{
+			id: 3,
+			viewed: false,
+			title: "Loja BadBons",
+			time: "20min",
+			message: "Camisas Novas já estão liberadas, confira!"
+		},
+		{
+			id: 4,
+			viewed: true,
+			title: "Equipe BadBons",
+			time: "20min",
+			message: "Aula de Reposição para a Tijuca, Dia 10 das 9:00 às 10:00"
+		},
+		{
+			id: 5,
+			viewed: false,
+			title: "NDB NEWS",
+			time: "10 min",
+			message: "Quartas de Final - Vencedor Igor Coelho"
+		},
+		{
+			id: 6,
+			viewed: false,
+			title: "Torneio Amigo",
+			time: "15min",
+			message: "Final - Vencedor Natan Roldão"
+		},
+		{
+			id: 7,
+			viewed: false,
+			title: "Loja BadBons",
+			time: "20min",
+			message: "Camisas Novas já estão liberadas, confira!"
+		},
+		{
+			id: 8,
+			viewed: true,
+			title: "Equipe BadBons",
+			time: "20min",
+			message: "Aula de Reposição para a Tijuca, Dia 10 das 9:00 às 10:00"
+		},
+		{
+			id: 9,
+			viewed: false,
+			title: "NDB NEWS",
+			time: "10 min",
+			message: "Quartas de Final - Vencedor Igor Coelho"
+		},
+		{
+			id: 10,
+			viewed: false,
+			title: "Torneio Amigo",
+			time: "15min",
+			message: "Final - Vencedor Natan Roldão"
+		},
+		{
+			id: 11,
+			viewed: false,
+			title: "Loja BadBons",
+			time: "20min",
+			message: "Camisas Novas já estão liberadas, confira!"
+		},
+		{
+			id: 12,
+			viewed: true,
+			title: "Equipe BadBons",
+			time: "20min",
+			message: "Aula de Reposição para a Tijuca, Dia 10 das 9:00 às 10:00"
+		},
+		{
+			id: 13,
+			viewed: false,
+			title: "NDB NEWS",
+			time: "10 min",
+			message: "Quartas de Final - Vencedor Igor Coelho"
+		},
+		{
+			id: 14,
+			viewed: false,
+			title: "Torneio Amigo",
+			time: "15min",
+			message: "Final - Vencedor Natan Roldão"
+		},
+		{
+			id: 15,
+			viewed: false,
+			title: "Loja BadBons",
+			time: "20min",
+			message: "Camisas Novas já estão liberadas, confira!"
+		}
+	]
+};
 
-export const listNotifications2 = [
-	{
-		id: 16,
-		viewed: true,
-		title: "TESTE",
-		time: "??/??/????",
-		message: "previa da notificacao"
+export const listNotifications2 = {
+	pagination: {
+		nextPage: false,
+		totalPages: 2
 	},
-	{
-		id: 17,
-		viewed: true,
-		title: "TESTE",
-		time: "??/??/????",
-		message: "previa da notificacao"
-	},
-	{
-		id: 18,
-		viewed: true,
-		title: "TESTE",
-		time: "??/??/????",
-		message: "previa da notificacao"
-	},
-	{
-		id: 19,
-		viewed: true,
-		title: "TESTE",
-		time: "??/??/????",
-		message: "previa da notificacao"
-	},
-	{
-		id: 20,
-		viewed: true,
-		title: "TESTE",
-		time: "??/??/????",
-		message: "previa da notificacao"
-	},
-	{
-		id: 21,
-		viewed: true,
-		title: "TESTE",
-		time: "??/??/????",
-		message: "previa da notificacao"
-	},
-	{
-		id: 22,
-		viewed: true,
-		title: "TESTE",
-		time: "??/??/????",
-		message: "previa da notificacao"
-	},
-	{
-		id: 23,
-		viewed: true,
-		title: "TESTE",
-		time: "??/??/????",
-		message: "previa da notificacao"
-	},
-	{
-		id: 24,
-		viewed: true,
-		title: "TESTE",
-		time: "??/??/????",
-		message: "previa da notificacao"
-	},
-	{
-		id: 25,
-		viewed: true,
-		title: "TESTE",
-		time: "??/??/????",
-		message: "previa da notificacao"
-	},
-	{
-		id: 26,
-		viewed: true,
-		title: "TESTE",
-		time: "??/??/????",
-		message: "previa da notificacao"
-	},
-	{
-		id: 27,
-		viewed: true,
-		title: "TESTE",
-		time: "??/??/????",
-		message: "previa da notificacao"
-	},
-	{
-		id: 28,
-		viewed: true,
-		title: "TESTE",
-		time: "??/??/????",
-		message: "previa da notificacao"
-	},
-	{
-		id: 29,
-		viewed: true,
-		title: "TESTE",
-		time: "??/??/????",
-		message: "previa da notificacao"
-	},
-	{
-		id: 30,
-		viewed: true,
-		title: "TESTE",
-		time: "??/??/????",
-		message: "previa da notificacao"
-	},
-];
+	data: [
+		{
+			id: 16,
+			viewed: true,
+			title: "TESTE",
+			time: "??/??/????",
+			message: "previa da notificacao"
+		},
+		{
+			id: 17,
+			viewed: true,
+			title: "TESTE",
+			time: "??/??/????",
+			message: "previa da notificacao"
+		},
+		{
+			id: 18,
+			viewed: true,
+			title: "TESTE",
+			time: "??/??/????",
+			message: "previa da notificacao"
+		},
+		{
+			id: 19,
+			viewed: true,
+			title: "TESTE",
+			time: "??/??/????",
+			message: "previa da notificacao"
+		},
+		{
+			id: 20,
+			viewed: true,
+			title: "TESTE",
+			time: "??/??/????",
+			message: "previa da notificacao"
+		},
+		{
+			id: 21,
+			viewed: true,
+			title: "TESTE",
+			time: "??/??/????",
+			message: "previa da notificacao"
+		},
+		{
+			id: 22,
+			viewed: true,
+			title: "TESTE",
+			time: "??/??/????",
+			message: "previa da notificacao"
+		},
+		{
+			id: 23,
+			viewed: true,
+			title: "TESTE",
+			time: "??/??/????",
+			message: "previa da notificacao"
+		},
+		{
+			id: 24,
+			viewed: true,
+			title: "TESTE",
+			time: "??/??/????",
+			message: "previa da notificacao"
+		},
+		{
+			id: 25,
+			viewed: true,
+			title: "TESTE",
+			time: "??/??/????",
+			message: "previa da notificacao"
+		},
+		{
+			id: 26,
+			viewed: true,
+			title: "TESTE",
+			time: "??/??/????",
+			message: "previa da notificacao"
+		},
+		{
+			id: 27,
+			viewed: true,
+			title: "TESTE",
+			time: "??/??/????",
+			message: "previa da notificacao"
+		},
+		{
+			id: 28,
+			viewed: true,
+			title: "TESTE",
+			time: "??/??/????",
+			message: "previa da notificacao"
+		},
+		{
+			id: 29,
+			viewed: true,
+			title: "TESTE",
+			time: "??/??/????",
+			message: "previa da notificacao"
+		},
+		{
+			id: 30,
+			viewed: true,
+			title: "TESTE",
+			time: "??/??/????",
+			message: "previa da notificacao"
+		},
+	]
+};
